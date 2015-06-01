93543d0b109617375638e0a10de3aa1bf628faff
CrazyAlbert <crazy.albert1301@gmail.com>
2015-06-01 14:18:22 -0600

    'Update'

---
+++ app/views/WickedPdf.config.rb
@@ -0,0 +1,6 @@
+{
+  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+  #x:layout => "pdf.html",
+  #:wkhtmltopdf => 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+  exe_path: 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+}
